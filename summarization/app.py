92436f04b9697e619ac3337d7a7479886017b29b
aditya.singh <aditya.singh@tataiq.com>
2023-12-12 04:52:14 +0000

    data summary

--- summarization/app.py
+++ summarization/app.py
@@ -41,7 +41,7 @@ def get_service_address(consul_client,service_name,env):
             services=consul_client.catalog.service(service_name)[1]
             print(services)
             for i in services:
-                if env == i["ServiceID"].split("-")[:-1]:
+                if env == i["ServiceID"].split("-")[-1]:
                     return i
         except:
             time.sleep(10)
